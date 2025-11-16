c2b0bc242bc7a5f8f8b0e304779900da82f9c572
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-11-16 10:16:45 +0100

    fix

--- includes/Blocks.php
+++ includes/Blocks.php
@@ -39,16 +39,8 @@ class Blocks
             }
 
             register_block_type(plugin_dir_path(__DIR__) . 'build/blocks/' . $block, $args);
-
-            // load_plugin_textdomain('evangelische-termine-ausgeben', false, dirname(plugin_basename(__DIR__)) . 'languages');
-
             $script_handle = generate_block_asset_handle('evangelische-termine/' . $block, 'editorScript');
-            // wp_set_script_translations($script_handle, 'evangelische-termine-ausgeben', plugin_dir_path(__DIR__) . 'languages');
-            //error log something
-            error_log('script handle: ' . $script_handle);
-        }
 
-        // Register global styles and scripts here.
-        // wp_enqueue_style('evangelische-termine-ausgeben');
+        }
     }
 }
