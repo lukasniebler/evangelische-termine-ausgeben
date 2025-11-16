5174e4ea7e2850c12dc49eca622be9389e228a21
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2025-11-16 09:30:32 +0100

    feat(render) - Add Render

---
+++ includes/BlockRender.php
@@ -0,0 +1,223 @@
+<?php
+
+namespace LN\EvangelischeTermineAusgeben;
+
+defined('ABSPATH') || exit;
+
+use WP_Error;
+use WP_REST_Request;
+
+class BlockRender
+{
+    private array $placeTypeMap = [];
+    private bool $placeTypesLoaded = false;
+
+    public function render_block_blueprint(array $attributes = []): string
+    {
+        $defaults = [
+            'id' => '',
+            'limit' => 10,
+            'eventType' => '',
+            'people' => '',
+            'placeType' => '',
+            'searchText' => '',
+        ];
+
+        $attributes = wp_parse_args($attributes, $defaults);
+        $queryArgs = $this->build_query_args($attributes);
+
+        $events = $this->fetch_events($queryArgs);
+        if (is_wp_error($events)) {
+            return $this->render_wrapper(
+                sprintf(
+                    '<p class="ln-eta-error">%s</p>',
+                    esc_html($events->get_error_message())
+                )
+            );
+        }
+
+        if (!is_array($events)) {
+            return $this->render_wrapper('');
+        }
+
+        $events = $this->filter_by_place_type($events, $attributes['placeType']);
+        $events = $this->filter_by_search($events, $attributes['searchText']);
+
+        if (empty($events)) {
+            $content = sprintf(
+                '<h2>%1$s</h2><p>%2$s</p>',
+                esc_html__('Events', 'evangelische-termine-ausgeben'),
+                esc_html__('Keine Veranstaltungen gefunden.', 'evangelische-termine-ausgeben')
+            );
+            return $this->render_wrapper($content);
+        }
+
+        $items = array_map(function ($event) {
+            $veranstaltung = $event['Veranstaltung'] ?? [];
+            $title = isset($veranstaltung['_event_TITLE']) ? esc_html($veranstaltung['_event_TITLE']) : '';
+            $datum = isset($veranstaltung['DATUM']) ? esc_html($veranstaltung['DATUM']) : '';
+            $startTime = '';
+            if (!empty($veranstaltung['START_UHRZEIT'])) {
+                $startTime = esc_html(str_replace('.', ':', $veranstaltung['START_UHRZEIT']));
+            }
+
+            $dt = sprintf('<dt>%s</dt>', $title ? '<strong>' . $title . '</strong>' : '');
+            $dateMarkup = $datum ? sprintf('<dd><br /><span>%s</span></dd>', $datum) : '<dd></dd>';
+            $timeMarkup = $startTime ? sprintf('<dd><span>%s</span></dd>', $startTime) : '<dd></dd>';
+
+            return $dt . $dateMarkup . $timeMarkup;
+        }, $events);
+
+        $content = sprintf(
+            '<h2>%1$s</h2><dl>%2$s</dl>',
+            esc_html__('Events', 'evangelische-termine-ausgeben'),
+            implode('', $items)
+        );
+
+        return $this->render_wrapper($content);
+    }
+
+    private function build_query_args(array $attributes): array
+    {
+        $params = [
+            'endpoint' => 'events',
+        ];
+
+        if (!empty($attributes['id'])) {
+            $params['vid'] = sanitize_text_field($attributes['id']);
+        }
+
+        $limit = isset($attributes['limit']) ? (int) $attributes['limit'] : 0;
+        if ($limit > 0) {
+            $params['itemsPerPage'] = $limit;
+        }
+
+        foreach (['eventType' => 'eventtype', 'people' => 'people', 'placeType' => 'placetype'] as $attr => $param) {
+            if (!empty($attributes[$attr])) {
+                $params[$param] = sanitize_text_field($attributes[$attr]);
+            }
+        }
+
+        return $params;
+    }
+
+    private function fetch_events(array $queryArgs)
+    {
+        $request = new WP_REST_Request('GET', '/evangelische-termine-ausgeben/v1/data');
+        foreach ($queryArgs as $key => $value) {
+            $request->set_param($key, $value);
+        }
+
+        $response = rest_do_request($request);
+        if (is_wp_error($response)) {
+            return $response;
+        }
+
+        if ($response instanceof WP_Error) {
+            return $response;
+        }
+
+        $data = $response->get_data();
+
+        if (!is_array($data)) {
+            return new WP_Error('ln_eta_invalid_response', __('Fehlerhafte Antwort vom Server.', 'evangelische-termine-ausgeben'));
+        }
+
+        return $data;
+    }
+
+    private function filter_by_place_type(array $events, string $placeType): array
+    {
+        if ($placeType === '') {
+            return $events;
+        }
+
+        $label = $this->get_place_type_label($placeType);
+        if (!$label) {
+            return $events;
+        }
+
+        return array_values(array_filter($events, function ($event) use ($label) {
+            return isset($event['Veranstaltung']['_place_KAT'])
+                && $event['Veranstaltung']['_place_KAT'] === $label;
+        }));
+    }
+
+    private function filter_by_search(array $events, string $searchText): array
+    {
+        $searchText = trim(
+            wp_strip_all_tags(
+                $searchText
+            )
+        );
+
+        if ($searchText === '') {
+            return $events;
+        }
+
+        $needle = mb_strtolower($searchText);
+
+        return array_values(array_filter($events, function ($event) use ($needle) {
+            $veranstaltung = $event['Veranstaltung'] ?? [];
+            $haystack = [];
+
+            foreach (['_event_TITLE', '_event_SHORT_DESCRIPTION', '_event_LONG_DESCRIPTION'] as $key) {
+                if (!empty($veranstaltung[$key])) {
+                    $haystack[] = mb_strtolower((string) $veranstaltung[$key]);
+                }
+            }
+
+            foreach ($haystack as $text) {
+                if (str_contains($text, $needle)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }));
+    }
+
+    private function get_place_type_label(string $placeType): ?string
+    {
+        if ($placeType === '') {
+            return null;
+        }
+
+        if (!$this->placeTypesLoaded) {
+            $this->load_place_types();
+        }
+
+        return $this->placeTypeMap[$placeType] ?? null;
+    }
+
+    private function load_place_types(): void
+    {
+        $this->placeTypesLoaded = true;
+
+        $request = new WP_REST_Request('GET', '/evangelische-termine-ausgeben/v1/data');
+        $request->set_param('endpoint', 'placetype');
+
+        $response = rest_do_request($request);
+        if (is_wp_error($response)) {
+            return;
+        }
+
+        $data = $response->get_data();
+        if (!is_array($data)) {
+            return;
+        }
+
+        foreach ($data as $item) {
+            if (isset($item['id'], $item['name'])) {
+                $this->placeTypeMap[(string) $item['id']] = (string) $item['name'];
+            }
+        }
+    }
+
+    private function render_wrapper(string $content): string
+    {
+        $wrapperAttributes = get_block_wrapper_attributes();
+        return sprintf('<div %s>%s</div>', $wrapperAttributes, $content);
+    }
+}
+
--- includes/Blocks.php
+++ includes/Blocks.php
@@ -6,8 +6,11 @@ defined('ABSPATH') || exit;
 
 class Blocks
 {
+    private BlockRender $renderer;
+
     public function __construct()
     {
+        $this->renderer = new BlockRender();
         add_action('init', [$this, 'evangelische_termine_ausgeben_elements_block_init']);
     }
 
@@ -29,7 +32,13 @@ class Blocks
         ];
 
         foreach ($blocks as $block) {
-            register_block_type(plugin_dir_path(__DIR__) . 'build/blocks/' . $block);
+            $args = [];
+
+            if ($block === 'block-blueprint') {
+                $args['render_callback'] = [$this->renderer, 'render_block_blueprint'];
+            }
+
+            register_block_type(plugin_dir_path(__DIR__) . 'build/blocks/' . $block, $args);
 
             // load_plugin_textdomain('evangelische-termine-ausgeben', false, dirname(plugin_basename(__DIR__)) . 'languages');
 
